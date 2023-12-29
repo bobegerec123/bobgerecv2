4fd72423b13dc2b4d8b4eeec56c6ead387654f90
bobegerec123 <124171019+bobegerec123@users.noreply.github.com>
2023-12-29 22:02:43 +0400

    АЛО ГИТ АЛО

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import random
 import math
+
+from pygame.sprite import Group
+from Sprites.MapSprite import Wall, Ground
+
+import Sprites.MapSprite
 
 
 def lenght(x1, y1, x2, y2) -> float:
@@ -6,3 +12,19 @@ def lenght(x1, y1, x2, y2) -> float:
     b = (y1 - y2) ** 2
     c = math.sqrt(a + b)
     return c
+
+
+def get_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    return r, g, b
+
+
+def get_lenght(x1, y1, x2, y2):
+    x = (x1 - x2) ** 2
+    y = math.pow(y1 - y2, 2)
+    return math.sqrt(x + y)
+
+
